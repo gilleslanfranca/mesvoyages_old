567768a30bac97e12ddca6ae43b133ec3ff91a1d
Gilles Lanfranca <lanfranca@gmx.de>
2021-10-17 11:26:54 +0200

    Phase 3 : plusieurs pages

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -10,20 +10,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 /**
- * Description of AcceuilController
+ * Description of VoyagesController
  *
  * @author lanfr
  */
-class AcceuilController extends AbstractController{
+class VoyagesController extends AbstractController{
     //put your code here
 
     /**
-     * @Route ("/", name="acceuil")
+     * @Route ("/voyages", name="voyages")
      * @return Response
      */
     public function index(): Response{
-        return $this->render("pages/acceuil.html.twig");
-        
+        return $this->render("pages/voyages.html.twig");
     }
 }
